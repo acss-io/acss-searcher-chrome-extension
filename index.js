ca8ad91024ced5703d256c4555d113fef30370ed
Tom Wu <tom76kimo@gmail.com>
2017-10-28 17:20:31 +0800

    Add basic layouts

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const rules = require('atomizer/src/rules');
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Rules = require('atomizer/src/rules')
 
-const Results = props => {};
+const StyleEntry = ({ matcher, styleName, argumentKey, argumentValue }) => {
+    return (
+        <li>{`${matcher}(${argumentKey}) ${styleName}: ${argumentValue}`}</li>
+    )
+}
+
+const ResultsEntry = ({ data }) => {
+    console.log('===data', data)
+    const {
+        name,
+        arguments: { '0': argument = {} } = [],
+        matcher,
+        styles,
+    } = data
+    const styleName = Object.keys(styles)[0]
+    return (
+        <div>
+            <h3>{name}</h3>
+            <ul>
+                {Object.keys(argument).map(key => {
+                    return (
+                        <StyleEntry
+                            key={`style-entry-${key}`}
+                            matcher={matcher}
+                            styleName={styleName}
+                            argumentKey={key}
+                            argumentValue={argument[key]}
+                        />
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+const Results = ({ searchText }) => {
+    const regex = new RegExp(`${searchText}`, 'i')
+    const result = Rules.filter(rule => {
+        return rule.name.search(regex) > -1
+    })
+    return (
+        <div className="Mah(450px) Ov(a)">
+            {result.map((entry, index) => {
+                return <ResultsEntry data={entry} key={`result-${index}`} />
+            })}
+        </div>
+    )
+}
 
 class App extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {};
-        this.onTextChange = this.onTextChange.bind(this);
+        super(props)
+        this.state = {}
+        this.onTextChange = this.onTextChange.bind(this)
     }
     render() {
+        const { searchText } = this.state
         return (
-            <div className="Px(10px) Py(6px) H(500px)">
+            <div className="Px(10px) Py(6px)">
                 <div className="W(300px)">
                     <input
                         className="W(100%) Bdrs(3px) Bgc(white) Bdc(#dbdbdb) Bd Fz(1rem) Lh(1.5) Px(10px) Py(6px) Bxz(bb) C(#363636)"
@@ -21,15 +69,17 @@ class App extends React.Component {
                         onChange={this.onTextChange}
                     />
                 </div>
+                <Results searchText={searchText} />
             </div>
-        );
+        )
     }
 
     onTextChange(event) {
+        const { target: { value = '' } = {} } = event
         this.setState({
-            searchText: event.target.value.trim()
-        });
+            searchText: value.trim(),
+        })
     }
 }
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+ReactDOM.render(<App />, document.querySelector('#main'))
